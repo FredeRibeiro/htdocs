a68a9b6a7aa1477ee41223dc15a89916f5673836
Frederico Lima Ribeiro <40181768+FredeRibeiro@users.noreply.github.com>
2018-08-02 13:19:38 -0300

    .

--- DAO/index.php
+++ DAO/index.php
@@ -1,36 +1,34 @@
-<?php
-
+<?php 
 require_once("config.php");
-
-//Carrega um usuario.
+//Carrega um usuário
 //$root = new Usuario();
-//$root->loadById(3);
+//$root->loadbyId(3);
 //echo $root;
-//////////////////////////////////////////////////////////////////////////
-//Carrega uma lista de usuarios;
+//Carrega uma lista de usuários
 //$lista = Usuario::getList();
 //echo json_encode($lista);
-//////////////////////////////////////////////////////////////////////////
-//Carrega uma lista de usuarios buscando pelo login
-//$search = Usuario::search("");
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
 //echo json_encode($search);
-//////////////////////////////////////////////////////////////////////////
-//Carrega um unsuario usando login e senha
+//carrega um usuário usando o login e a senha
 //$usuario = new Usuario();
-//$usuario->login("root","123456");
+//$usuario->login("root", "!@#$");
 //echo $usuario;
-//////////////////////////////////////////////////////////////////////////
-//Utilizando a Procedure para inserir dados. 
- //$aluno = new Usuario("Frederico", "frede1089");
- //$aluno->insert();
- //echo $aluno;
-//////////////////////////////////////////////////////////////////////////
-//Alterando os dados dentro do banco. 
+/*
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+/*
+//Alterar um usuário
 $usuario = new Usuario();
-
+$usuario->loadById(8);
+$usuario->update("professor", "!@#$%¨&*");
+echo $usuario;
+*/
+$usuario = new Usuario();
 $usuario->loadById(7);
-
-$usuario->update("Frede", "301089");
-
+$usuario->delete();
 echo $usuario;
  ?>
